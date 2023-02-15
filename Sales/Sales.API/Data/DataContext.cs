8d727bc2585316058a919315fdccede66fe2521f
Jhon Munoz <jhonmunoz225205@correo.itm.edu.co>
2023-02-14 19:08:39 -0500

    Add dataContext

---
+++ Sales/Sales.API/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class DataContext : DbContext //Debo de heredar e instalar el entity framework. para conectarme a la db necesito constructor
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) //ctor
+        {
+
+        }
+
+        //para mapear mis tablas hay que crear una propiedad de tipo DbSet (Debo importar la clase)
+
+        public DbSet<Country> Countries { get; set; }
+
+        //Para validar duplicados es creando un indice unico
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique(); //se usa expresion lambda
+        }
+    }
+}
---
+++ Sales/Sales.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities  // las entidades las combierto en una base de datos
+{
+    public class Country
+    {
+        public int Id { get; set; } //codigo del pais
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //El cero de reemplaza por el nombre del display o por el maxleng
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1}")] // el 1 se reemplaza por la cantidad de maxlengt
+        [Display(Name = "País")]
+        public string Name { get; set; } = null!;
+    }
+}
